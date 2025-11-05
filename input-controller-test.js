4b43e8a42d846e3388d266b29f598ed30083b88d
yh3728 <alinamalinagames@gmail.com>
2025-11-05 14:14:23 +0300

    simple bindActions, enableAction, disableAction, attach, detach, isActionActive realizations

--- input-controller-test.js
+++ input-controller-test.js
@@ -1,35 +1,66 @@
-btnsDiv = document.getElementById("input-test");
-btns = btnsDiv.querrySelectorAll('*');
+
+// btnsDiv = document.getElementById("input-test");
+// btns = btnsDiv.querrySelectorAll('*');
+
 canvas = document.getElementById("test-canvas");
 ctx = canvas.getContext("2d");
-document.addEventListener('input-controller:activate');
-document.addEventListener('input-controller:deactivate');
+let x = 100;
+let y = 100;
+const WIDTH = 60;
+const HEIGHT = 60;
+const SPEED = 15;
 
+let controller;
 
-let actionsToBind = [
+let actionsToBind = 
     {
         "left":
         {
-            keys: [37,65]
+            keys: [37,65],
         },
         "right":
         {
-            keys: [39,68]
+            keys: [39,68],
         }
     }
-];
+;
+
+function moveLeft() {
+    x -= SPEED;
+}
+
+function moveRight(){
+    x += SPEED;
+}
 
 function init(){
-    let controller = InputController(actionsToBind, canvas);
+    controller = new InputController(actionsToBind, canvas);
+    
 }
+
+// вешаем слушатели и обработчики 
+
+canvas.addEventListener(
+    controller.ACTION_ACTIVATED,
+    (e) => {
+        console.log("activate" + e.detail.action)
+    }
+)
+
+canvas.addEventListener(
+    controller.ACTION_DEACTIVATED,
+    (e) => {
+        console.log("deactivate" + e.detail.action)
+    }
+)
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+    controller.focused = (document.activeElement === canvas)
+    ctx.fillStyle = "red";
+    ctx.fillRect(x, y, WIDTH, HEIGHT)
     requestAnimationFrame(animate);
 }
-
-
 
 init();
 animate();
--- input-controller.js
+++ input-controller.js
@@ -1,59 +1,140 @@
-
-// actionsToBind <object> : Объект со списком активностей и, соответствующих им, кодов клавиш клавиатуры.
-// 	Пример:
-// {
-// 	"left": { // название активности
-// 		keys: [37,65], // список кодов кнопок соответствующих активности
-// enabled: false // отключенная активность
-// 	},
-// 	"right": {
-// 		keys: [39,68]
-// 	},
-// 	...
-// 	}
-
-
 class InputController {
-
-    target;
-    enabled;
-    focused;
-    ACTION_ACTIVATED;
-    ACTION_DEACTIVATED;
+    actions = {};
+    target = document;
+    enabled = true;
+    focused = true;
+    ACTION_ACTIVATED = "input-controller:action-activated";
+    ACTION_DEACTIVATED = "input-controller:action-deactivated";
 
     constructor(actionsToBind, target){
         this.bindActions(actionsToBind);
         this.target = target;
     }
 
+    /*
+    Добавляет в контроллер переданные активности.
+    actionsToBind <object> : Объект со списком активностей и, соответствующих им, кодов клавиш 
+    клавиатуры.
+    */
+
     bindActions(actionsToBind){
-        
+        for (const key of Object.keys(actionsToBind)){
+            if (!this.actions.hasOwnProperty(key)){
+                this.actions[key] = actionsToBind[key];
+                if (!this.actions[key].hasOwnProperty("enabled")){
+                    this.actions[key].enabled = false;
+                }
+            }
+            else {
+                //TODO добавить в keys коды которых тут нет 
+            }
+        }
     }
+
+    addListeners(){
+        for (let action in this.actions){
+            
+        }
+    }
+
+
+    /*
+    Включает объявленную активность - включает генерацию событий для этой активности при изменении 
+    её статуса. Если включено:
+    при проверке активности через isActionActive 
+    возвращает актуальное состояние активности (напр. для клавиатуры нажата кнопка или нет), 
+    иначе всегда возвращает false
+    может генерировать событие при изменении состояния действия, иначе событие не генерируется
+    actionName <string> : название активности
+    */
 
     enableAction(actionName){
+        if (this.enabled && this.focused){
+            if (!this.actions.hasOwnProperty(actionName)){
+                return;
+            }
+            if (!this.actions[actionName].enabled)
+                this.actions[actionName].enabled = true;
 
+                this.dispatchEvent(actionName, "activate")
+        }
     }
+
+    /*
+        Деактивирует объявленную активность - выключает генерацию событий для этой активности. 
+        После чего при проверке доступности этой активности через isActionActive всегда возвращает 
+        false. А также при изменении состояния активности, события не генерируются.
+        actionName <string> : название активности
+    */
 
     disableAction(actionName){
-
+        if (this.enabled && this.focused){
+            if (!this.actions.hasOwnProperty(actionName)){
+                return;
+            }
+            if (this.actions[actionName].enabled)
+                this.actions[actionName].enabled = false;
+                this.dispatchEvent(actionName, "deactivate")
+        }
     }
+
+    
+    /*
+    диспатчит событие нужного типа
+    */
+    dispatchEvent(action, type) {
+        eventName = (type === "activate")? this.ACTION_ACTIVATED : this.ACTION_DEACTIVATED;
+        const event = new CustomEvent(
+            eventName,
+            {
+                detail: {
+                    action: action,
+                }
+            }
+        );
+        this.target.dispatchEvent(event);
+    }
+
+    /*
+    Нацеливает контроллер на переданный DOM-элемент (вешает слушатели).
+    target <element> :  DOM-элемент на котором слушает события клавиатуры и диспачит свои события
+    dontEnable <bool> (опционально): Если передано true - не активирует контроллер.
+    */
 
     attach(target, dontEnable){
-
+        this.target = target;
+        this.enabled = !dontEnable;
     }
+
+    /*
+    Отцепляет контроллер от активного DOM-элемента и деактивирует контроллер.
+    */
 
     detach(){
         this.target = null;
         this.enabled = false;
-
     }
+
+    /*
+    Проверяет активирована ли переданная активность в контроллере 
+    ( напр. для клавиатуры: зажата ли одна из соответствующих этой активности кнопок)
+    */
 
     isActionActive(action){
-        
+        if (this.actions.hasOwnProperty(action))
+            return this.actions[action].enabled;
+        else 
+            return false;
     }
+
+    /*
+    Метод для источника ввода клавиатура. Проверяет нажата ли переданная кнопка в контроллере
+    keyCode <int> : Код кнопки для проверки
+    */
 
     isKeyPressed(keyCode){
         
     }
 
 }
+
